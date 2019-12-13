90772c4727069b78504e242d9e1b651981e966f1
Sabrina Becker <sbecka8@gmail.com>
2019-12-13 16:53:47 -0500

    Add states to components and pass them as props

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ import FilterableList from './FilterableList/FilterableList';
 
 //pass files prop along to FilterableList
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      filterOption: 'All'
+    };
+  }
   render() {
     return (
       <div className='App'>
-        <SearchBar />
-        <FilterableList files={this.props.files} />
+        <SearchBar 
+          searchTerm={this.state.searchTerm} 
+          filterOption={this.state.filterOption}/>
+        <FilterableList 
+          files={this.props.files}
+          searchTerm={this.state.searchTerm} 
+          filterOption={this.state.filterOption}/>
       </div>
     );
   }
